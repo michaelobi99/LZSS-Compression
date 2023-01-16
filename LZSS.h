a4a84ac67f15261ffa5b00d61bfaddd0af65ea7d
Michael <michaelobi54@gmail.com>
2023-01-16 16:57:47 +0100

    implemented decoding

---
+++ LZSS.h
@@ -0,0 +1,202 @@
+#pragma once
+#include "BitIO.h"
+#include <cstring>
+#include <sstream>
+
+constexpr int INDEX_BIT_COUNT = 14;
+constexpr int LENGTH_BIT_COUNT = 6;
+constexpr int WINDOW_SIZE = (1 << INDEX_BIT_COUNT);
+constexpr int LOOK_AHEAD_SIZE = (1 << LENGTH_BIT_COUNT);
+constexpr int BYTE = 8;
+constexpr int TREE_ROOT = WINDOW_SIZE;
+constexpr int UNUSED = 0;
+constexpr int END_OF_STREAM = 0;
+
+#define MOD_WINDOW(value) ((value) & (WINDOW_SIZE - 1))
+
+unsigned char window[WINDOW_SIZE];
+	
+
+struct Tree {
+	int parent;
+	int largerChild;
+	int smallerChild;
+};
+
+Tree tree[WINDOW_SIZE + 1];
+
+void contractNode(int oldNode, int newNode) {
+	tree[newNode].parent = tree[oldNode].parent;
+	if (tree[tree[oldNode].parent].largerChild == oldNode)
+		tree[tree[oldNode].parent].largerChild = newNode;
+	else
+		tree[tree[oldNode].parent].smallerChild = newNode;
+	tree[oldNode].parent = UNUSED;
+}
+
+int findNextNode(int node) {
+	int next = tree[node].smallerChild;
+	while (tree[next].largerChild != UNUSED)
+		next = tree[next].largerChild;
+	return next;
+}
+
+void replaceNode(int oldNode, int newNode) {
+	int parent = tree[oldNode].parent;
+	if (tree[parent].smallerChild == oldNode)
+		tree[parent].smallerChild = newNode;
+	else
+		tree[parent].largerChild = newNode;
+	tree[newNode] = tree[oldNode];
+	tree[tree[newNode].smallerChild].parent = newNode;
+	tree[tree[newNode].largerChild].parent = newNode;
+	tree[oldNode].parent = UNUSED;
+}
+
+void deleteString(int position) {
+	if (tree[position].parent == UNUSED)
+		return;
+	if (tree[position].largerChild == UNUSED)
+		contractNode(position, tree[position].smallerChild);
+	else if (tree[position].smallerChild == UNUSED)
+		contractNode(position, tree[position].largerChild);
+	else {
+		int replacementPosition = findNextNode(position);
+		deleteString(replacementPosition);
+		replaceNode(position, replacementPosition);
+	}
+}
+
+void addString(int stringPosition) {
+	//printf("%c", window[stringPosition]);
+	int i{ 0 }, testNode{ 0 }, delta{ 0 }, * child{ nullptr };
+	if (tree[TREE_ROOT].largerChild == UNUSED) {
+		tree[TREE_ROOT].largerChild = stringPosition;
+		tree[stringPosition].parent = TREE_ROOT;
+		tree[stringPosition].largerChild = UNUSED;
+		tree[stringPosition].smallerChild = UNUSED;
+	}
+	else {
+		testNode = tree[TREE_ROOT].largerChild;
+		for (;;) {
+			for (i = 0; i < LOOK_AHEAD_SIZE; ++i) {
+				delta = window[MOD_WINDOW(stringPosition + i)] - window[MOD_WINDOW(testNode + i)];
+				if (delta != 0)
+					break;
+			}
+			if (delta == 0) {
+				replaceNode(testNode, stringPosition);
+				break;
+			}
+			else if (delta > 0)
+				child = &tree[testNode].largerChild;
+			else
+				child = &tree[testNode].smallerChild;
+			if (*child == UNUSED) {
+				*child = stringPosition;
+				tree[stringPosition].parent = testNode;
+				tree[stringPosition].largerChild = UNUSED;
+				tree[stringPosition].smallerChild = UNUSED;
+				break;
+			}
+			testNode = *child;
+		}
+	}
+	
+}
+
+int getMatchLength(int currentPosition, int* matchPosition) {
+	int i{ 0 }, testNode{ 0 }, delta{ 0 }, matchLength{ 0 }, * child{ nullptr };
+	testNode = tree[TREE_ROOT].largerChild;
+	for (;;) {
+		//std::cout << "testNode = " << testNode << "\n";
+		for (i = 0; i < LOOK_AHEAD_SIZE; ++i) {
+			delta = window[MOD_WINDOW(currentPosition + i)] - window[MOD_WINDOW(testNode + i)];
+			if (delta != 0)
+				break;
+		}
+		if (i >= matchLength) {
+			matchLength = i;
+			*matchPosition = testNode;
+		}
+		if (delta == 0)
+			break;
+		else if (delta > 0)
+			child = &tree[testNode].largerChild;
+		else
+			child = &tree[testNode].smallerChild;
+		if (*child == UNUSED)
+			break;
+		testNode = *child;
+	}
+	return matchLength;
+}
+
+void compressFile(std::fstream& input, std::unique_ptr<stl::BitFile>& output) {
+	int i{ 0 }, c{ 0 }, lookAheadBytes{ 0 }, currentPosition{ 0 }, replaceCount{ 0 },
+		matchLength{ 0 }, matchPosition{ 0 };
+	for (i = 0; i < LOOK_AHEAD_SIZE; i++) {
+		c = input.get();
+		if (input.eof())
+			break;
+		window[currentPosition + i] = (unsigned char)c;
+	}
+	lookAheadBytes = i;
+	//initializeTree(currentPosition);
+	while (lookAheadBytes > 0) {
+		if (matchLength >= lookAheadBytes && lookAheadBytes == LOOK_AHEAD_SIZE)
+			matchLength = lookAheadBytes - 1;
+		if (matchLength <= 1) {
+			matchLength = 1;
+			stl::outputBit(output, 0);
+			stl::outputBits(output, (std::uint32_t)window[currentPosition], BYTE);
+		}
+		else {
+			stl::outputBit(output, 1);
+			stl::outputBits(output, (std::uint32_t)matchPosition, INDEX_BIT_COUNT);
+			stl::outputBits(output, (std::uint32_t)(matchLength), LENGTH_BIT_COUNT);
+		}
+		replaceCount = matchLength;
+		for (i = 0; i < replaceCount; ++i) {
+			deleteString(MOD_WINDOW(currentPosition + LOOK_AHEAD_SIZE));
+			c = input.get();
+			if (input.eof())
+				--lookAheadBytes;
+			else
+				window[MOD_WINDOW(currentPosition + LOOK_AHEAD_SIZE)] = (unsigned char)c;
+			addString(currentPosition);
+			currentPosition = MOD_WINDOW(currentPosition + 1);
+			//std::cout << matchPosition++ << ":" << matchLength << "\n";
+		}
+		if (lookAheadBytes)
+			matchLength = getMatchLength(currentPosition, &matchPosition);
+	}
+	stl::outputBit(output, 1);
+	stl::outputBits(output, (std::uint32_t)END_OF_STREAM, INDEX_BIT_COUNT + LENGTH_BIT_COUNT);
+}
+
+void expandFile(std::unique_ptr<stl::BitFile>& input, std::fstream& output) {
+	int i{ 0 }, currentPosition{ 0 }, c{ 0 }, matchLength{ 0 }, matchPosition{ 0 };
+	currentPosition = 0;
+	for (;;) {
+		if (stl::inputBit(input) == 0) {
+			c = (int)stl::inputBits(input, BYTE);
+			output.put(c);
+			window[currentPosition] = (unsigned char)c;
+			currentPosition = MOD_WINDOW(currentPosition + 1);
+			
+		}
+		else {
+			matchPosition = (int)stl::inputBits(input, INDEX_BIT_COUNT);
+			matchLength = (int)stl::inputBits(input, LENGTH_BIT_COUNT);
+			if (matchLength == END_OF_STREAM)
+				break;
+			for (i = 0; i < matchLength; i++) {
+				c = window[MOD_WINDOW(matchPosition + i)];
+				output.put(c);
+				window[currentPosition] = (unsigned char)c;
+				currentPosition = MOD_WINDOW(currentPosition + 1);
+			}
+		}
+	}
+}
---
+++ Source.cpp
@@ -0,0 +1,71 @@
+#include <filesystem>
+#include <chrono>
+#include <format>
+#include "LZSS.h"
+
+namespace fs = std::filesystem;
+
+struct Timer {
+public:
+	Timer() = default;
+	void Start() {
+		start = std::chrono::high_resolution_clock::now();
+	}
+	void Stop() {
+		stop = std::chrono::high_resolution_clock::now();
+	}
+	float time() {
+		elapsedTime = std::chrono::duration<float>(stop - start).count();
+		return elapsedTime;
+	}
+private:
+	float elapsedTime{};
+	std::chrono::time_point<std::chrono::high_resolution_clock> start, stop;
+};
+
+
+uintmax_t fileSize(fs::path const& path) {
+	auto lengthInbytes = fs::file_size(path);
+	return lengthInbytes;
+}
+
+
+int main() {
+	auto timer = Timer();
+	try {
+		std::fstream input(R"(..\LZSS\testFile1.txt)", std::ios_base::in | std::ios_base::binary);
+		if (!input.is_open())
+			exit(1);
+		auto output = stl::OpenOutputBitFile(R"(..\LZSS\testFile2.txt)");
+		std::cout << "compression started....\n";
+		timer.Start();
+		compressFile(input, output);
+		timer.Stop();
+		printf("\nFile compression complete\n");
+		printf("LZSS compression time = %f seconds\n\n", timer.time());
+		stl::closeOutputBitFile(output);
+
+		auto input1 = stl::OpenInputBitFile(R"(..\LZSS\testFile2.txt)");
+		std::fstream output1(R"(..\LZSS\testFile3.txt)", std::ios_base::out | std::ios_base::binary);
+		printf("Expansion started....\n");
+		timer.Start();
+		expandFile(input1, output1);
+		timer.Stop();
+		printf("\nFile expansion complete\n");
+		printf("LZSS expansion time = %f seconds\n\n", timer.time());
+		stl::closeInputBitFile(input1);
+		output1.close();
+
+		//print file sizes
+		std::cout << std::format("Original file size = {} bytes\n", fileSize(fs::path(R"(..\LZSS\testFile1.txt)")));
+		std::cout << std::format("Compressed file size = {} bytes\n", fileSize(fs::path(R"(..\LZSS\testFile2.txt)")));
+		std::cout << std::format("Expanded file size = {} bytes\n", fileSize(fs::path(R"(..\LZSS\testFile3.txt)")));
+	}
+	catch (stl::FileError const& error) {
+		std::cout << error.what();
+		std::cout << "File compression failed\n";
+	}
+	catch (...) {
+		std::cout << "An error occurred during compression or expansion\n";
+	}
+}
